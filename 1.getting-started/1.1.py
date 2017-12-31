a75d3f7d6c94e21e0437843da9618c2e18b2c8f1
isaac-philip <isaac.philip25@gmail.com>
2017-12-31 17:56:29 +0530

    added the 1st exercise code

---
+++ 1.getting-started/1.1.py
@@ -0,0 +1,10 @@
+'''
+    @created : 31st December, 2017
+    @author : iphilip
+    @python-v : 3.6+
+    Question present at [Running Python Interpreter](http://anandology.com/python-practice-book/getting-started.html#running-python-interpreter)
+    This module aims to achieve the sum of two numbers
+'''
+
+if __name__ == '__main__':
+    print(2+3)
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Python training exercises from  [Anandology](http://anandology.com/python-practice-book/index.html)
 
 ---
+
+1. Getting Started
